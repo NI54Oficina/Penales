bffb7a07a97d9c6aaf5b78a1e60da50aaaa893dd
Melania Miranda <melania.smc@hotmail.com>
2016-09-16 16:17:02 -0300

    salio el carrousellllllllll mother fucker.

--- states/selectRival.js
+++ states/selectRival.js
@@ -6,14 +6,14 @@ Selectplayer.prototype = {
     this.optionCount = 1;
     game.load.image('img-1', 'assets/images/test.png');
     game.load.image('img-2', 'assets/images/test2.png');
-    game.load.image('img-3', 'assets/images/test3.jpg');
-    game.load.image('img-4', 'assets/images/test4.jpg');
-    game.load.image('img-5', 'assets/images/test5.jpg');
-    game.load.image('img-6', 'assets/images/test6.jpg');
-    game.load.image('img-7', 'assets/images/test7.jpg');
-    game.load.image('img-8', 'assets/images/test8.jpg');
-    game.load.image('img-9', 'assets/images/test9.jpg');
-    game.load.image('img-10', 'assets/images/test4.jpg');
+    game.load.image('img-3', 'assets/images/test3.png');
+    game.load.image('img-4', 'assets/images/test4.png');
+    game.load.image('img-5', 'assets/images/test5.png');
+    game.load.image('img-6', 'assets/images/test6.png');
+    game.load.image('img-7', 'assets/images/test7.png');
+    game.load.image('img-8', 'assets/images/test8.png');
+    game.load.image('img-9', 'assets/images/test9.png');
+    game.load.image('img-10', 'assets/images/test10.png');
   },
 
   addMenuOption: function(text, callback) {
@@ -47,6 +47,7 @@ Selectplayer.prototype = {
   create: function(){
       self = this;
       indiceC=0;
+      i=0;
 
       game.add.sprite(0, 0, 'rival-bg');
 
@@ -75,13 +76,13 @@ Selectplayer.prototype = {
       img10=game.add.sprite(0, 0, 'img-10');
 
 
-      arrayCr=[img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
-      arrayCr.visible=false;
+      arrayCr=[img1, img2, img3,img4 , img5, img6, img7, img8, img9, img10];
+      //arrayCr.visible=false;
 
 
-      self.setTo5(img9);
+      self.setTo5(img10);
       self.setTo4(img3);
-      self.setTo3(img10);
+      self.setTo3(img9);
       self.setTo2(img2);
       self.setTo1(img1);
 
@@ -126,188 +127,178 @@ Selectplayer.prototype = {
 
   },
 
-  carrousel: function(){
 
-    s5= arrayCr[arrayCr.length-indiceC-1];
-    s5.position={x:350, y:400};
-    s5.alpha=.5;
-    s5.scale.setTo(.50,.50);
-    s5.inputEnabled = true;
-     s5.events.onInputDown.add(this.actionOnClick, s5);
+moveToRight: function(s){
 
-    s4= arrayCr[indiceC+2];
-    s4.position={x:750, y:400};
-    s4.alpha=.5;
-    s4.scale.setTo(.50,.50);
-     s4.inputEnabled = true;
-     s4.events.onInputDown.add(this.actionOnClick, s4);
+  console.log("MOVE TO RIGHT");
 
+  self.getID(s);
 
-    s3= arrayCr[arrayCr.length-indiceC-2];
-    s3.position={x:400, y:350};
-    s3.alpha=.7;
-    s3.scale.setTo(.75,.75);
-    s3.inputEnabled = true;
-     s3.events.onInputDown.add(this.actionOnClick, s3);
+  var carrousel=[];
+  var j=i;
 
-    s2= arrayCr[indiceC+1];
-    s2.position={x:650, y:350};
-    s2.alpha=.7;
-    s2.scale.setTo(.75,.75);
-    s2.inputEnabled = true;
-    s2.events.onInputDown.add(this.actionOnClick, s2);
+  while(carrousel.length <= 5) {
 
-    s1= arrayCr[indiceC];
-    s1.position={x:500, y:300};
-    this.world.bringToTop(s1);
+    if(j<arrayCr.length){
+
+      carrousel.push(j);
+
+    }else{
+
+      j=0;
+
+      carrousel.push(j);
+    }
+
+    j--;
+  }
+
+  console.log(carrousel);
 
 
+},
+  moveToLeft: function(s){
+    console.log("MOVE TO LEFT");
 
-    console.log(s2);
+    self.getID(s);
 
 
 
+    var carrousel=[];
+    var j=i;
 
-        //
-        // //imagen mas baja a la izquierda
-        // s5= game.add.sprite(300, 400, arrayimg[arrayimg.length-2]);
-        // s5.scale.setTo(.50,.50);
-        // s5.alpha=.3;
-        // s5.id=5;
-        //
-        // // imagen mas baja aa la derecha
-        // s4= game.add.sprite(800, 400, arrayimg[2]);
-        // s4.scale.setTo(.50,.50);
-        // s4.alpha=.3;
-        // s4.id=4;
-        //
-        // //imagen menos baja a la izquierda
-        // s3= game.add.sprite(400, 350, arrayimg[1]);
-        // s3.scale.setTo(.75,.75);
-        // s3.alpha=.5;
-        // s3.id=3;
-        //
-        // //imagen menos baja a la derecha
-        // s2= game.add.sprite(650, 350, arrayimg[arrayimg.length-1]);
-        // s2.scale.setTo(.75,.75);
-        // s2.alpha=.5;
-        // s2.id=2;
-        // s2.inputEnabled = true;
-        // s2.events.onInputDown.add(this.actionOnClick, s2);
-        // // console.log(img2.key);
-        //
-        //
-        // //imagen central
-        // s1= game.add.sprite(500, 300, arrayimg[0]);
-        // img1.id=1;
+    while(carrousel.length <= 2) {
 
-
-
-
-  },
-
-
-  actionOnClick: function(s){
-
-    for(var i=0; i<=arrayCr.length;i++){
-
-      if(arrayCr[i].key==s.key){
-        break;
+      if(j<arrayCr.length){
+        carrousel.push(j);
+      }else{
+        j=0;
+        carrousel.push(j);
       }
-
-    }
-    console.log(i);
-    console.log("step1");
-
-    if(i<= arrayCr.length){
-
-      console.log(i);
-
-        console.log("enter if");
-
-        self.setTo5(arrayCr[i+2]);
-        self.setTo4(arrayCr[i+1]);
-        self.setTo3(arrayCr[i+3]);
-        self.setTo2(arrayCr[i+4]);
-        self.setTo1(arrayCr[i]);
-
-
-
-      //  game.add.tween(target).to( s1.position, 2000, 'Linear');
-      //  game.add.tween(target.scale).to( {x:1, y:1}, 2000, 'Linear');
-
-       //tween.start();
-
+      j++;
 
 
     }
-  console.log("step2");
+     j=i;
+    while(carrousel.length <=4){
 
-  //  self.world.bringToTop(target);
+      if(j>0){
+        carrousel.push(--j);
+      }else{
+        j=arrayCr.length-1;
+          carrousel.push(j);
+      }
+    }
+
+    a= carrousel[0];
+    b= carrousel[1];
+    c= carrousel[3];
+    d= carrousel[2];
+    e= carrousel[4];
+
+    console.log(carrousel);
+
+
+
+      self.setTo5(arrayCr[e]);
+      self.setTo4(arrayCr[d]);
+      self.setTo3(arrayCr[c]);
+      self.setTo2(arrayCr[b]);
+      self.setTo1(arrayCr[a]);
+
+
+      //  var tweenA= game.add.tween(target).to( s1.position, 2000, 'Linear');
+      //  var tweenB =game.add.tween(target.scale).to( {x:1, y:1}, 2000, 'Linear');
+
+       //tweenA.start();
+     //tweenB.start();
+
+
 },
 
 
 setTo5: function(target){
 
-  if(typeof s5 !== 'undefined'){
-    s5.destroy()
-  }
   s5= target;
-  s5.position={x:350, y:400};
+
+  //s5.position={x:350, y:400};
   s5.alpha=.5;
-  s5.scale.setTo(.50,.50);
+  //s5.scale.setTo(.50,.50);
   s5.inputEnabled = true;
-   s5.events.onInputDown.add(this.actionOnClick,s5);
+  s5.events.onInputDown.add(this.moveToLeft,s5);
+
+  var tweenA= game.add.tween(target).to({x:350, y:400}, 500, 'Linear');
+  var tweenB =game.add.tween(target.scale).to( {x:.50, y:.50}, 500, 'Linear');
+
+  tweenA.start();
+  tweenB.start();
 
 },
 
 setTo4: function(target){
-  if(typeof s4 !== 'undefined'){
-    s4.destroy()
-  }
+
   s4= target;
-  s4.position={x:750, y:400};
+  //s4.position={x:750, y:400};
   s4.alpha=.5;
-  s4.scale.setTo(.50,.50);
+  //s4.scale.setTo(.50,.50);
    s4.inputEnabled = true;
-   s4.events.onInputDown.add(this.actionOnClick, s4);
+   s4.events.onInputDown.add(this.moveToLeft, s4);
+
+   var tweenA= game.add.tween(target).to({x:750, y:400}, 500, 'Linear');
+   var tweenB =game.add.tween(target.scale).to( {x:.50, y:.50}, 500, 'Linear');
+
+   tweenA.start();
+   tweenB.start();
+
 },
 
 setTo3: function(target){
 
-  if(typeof s3 !== 'undefined'){
-    s3.destroy()
-  }
   s3=target;
-  s3.position={x:400, y:350};
+  //s3.position={x:400, y:350};
   s3.alpha=.7;
-  s3.scale.setTo(.75,.75);
+  //s3.scale.setTo(.75,.75);
   s3.inputEnabled = true;
-  s3.events.onInputDown.add(this.actionOnClick, s3);
+  s3.events.onInputDown.add(this.moveToLeft, s3);
+
+  var tweenA= game.add.tween(target).to({x:400, y:350}, 500, 'Linear');
+  var tweenB =game.add.tween(target.scale).to( {x:.75, y:.75}, 500, 'Linear');
+
+  tweenA.start();
+  tweenB.start();
 },
 
 setTo2: function(target){
-  if(typeof s2 !== 'undefined'){
-    s2.destroy()
-  }
 
   s2= target;
-  s2.position={x:650, y:350};
+  //s2.position={x:650, y:350};
   s2.alpha=.7;
-  s2.scale.setTo(.75,.75);
+//  s2.scale.setTo(.75,.75);
   s2.inputEnabled = true;
-  s2.events.onInputDown.add(this.actionOnClick,s2);
+  s2.events.onInputDown.add(this.moveToLeft,s2);
+
+  var tweenA= game.add.tween(target).to({x:650, y:350}, 500, 'Linear');
+  var tweenB =game.add.tween(target.scale).to( {x:.75, y:.75}, 500, 'Linear');
+
+  tweenA.start();
+  tweenB.start();
 },
 
 setTo1: function(target){
-  if(typeof s1 !== 'undefined'){
-    s1.destroy()
-  }
-  //self.setResult(target);
+
   s1= target;
-  s1.position={x:500, y:300};
+  //s1.position={x:500, y:300};
   this.world.bringToTop(s1);
-  s1.scale.setTo(1,1);
+  //s1.scale.setTo(1,1);
+  s2.alpha=1;
+   var tweenA= game.add.tween(target).to({x:500, y:300}, 500, 'Linear');
+   var tweenB =game.add.tween(target.scale).to( {x:1, y:1}, 500, 'Linear');
+
+   tweenA.start();
+ tweenB.start();
+
+
+
 
 },
 
@@ -318,6 +309,21 @@ setResult: function(auxValue){
 
 getResult: function(){
   return result;
+},
+
+
+getID: function(s){
+  for( i=0; i<=arrayCr.length;i++){
+
+    if(arrayCr[i].key==s.key){
+      break;
+    }
+
+  };
+
+  console.log(i);
+
+  return i;
 },
 
 
