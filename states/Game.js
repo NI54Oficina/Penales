e2636c14402a7a570bf45c49d1c6cdeca7c82ffa
Fran <fran@ni54.com>
2016-10-31 16:13:50 -0300

    16:13 31-10

--- states/Game.js
+++ states/Game.js
@@ -177,17 +177,17 @@ drawPelota:function(){
 drawPlayer:function(){
 	player = game.add.sprite(318,210, 'pateador-local');
 	playerIPos=  new Phaser.Point();
-	playerIPos.x= 50;
+	playerIPos.x= 0;
 	playerIPos.y=350;
 	player.scale.setTo(1.3,1.3);
 	player.x=playerIPos.x;
 	player.y=playerIPos.y;
 	player.frame = 0;
 	//animacion player
-	player.animations.add('right', [04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37], 14, false);
-	player.animations.add('idle', [00,01,02,03,02,01], 2 , true);
+	player.animations.add('right', [04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37], 16, false);
+	player.animations.add('idle', [00], 2 , true);
 	player.animations.add('derrota', [38,39,40,41,42,43], 12 , false);
-	player.animations.add('festejo', [44,45,46,47,48,49,50,51,52,53,54], 12 , false);
+	player.animations.add('festejo', [44,45,46,47,48,49,50,51,52,53], 12 , false);
 	player.visible=true;
 	player.play("idle");
 },
@@ -480,7 +480,7 @@ updateCounter: function () {
   patear: function(){
     tweenPlayer = game.add.tween(player);
 
-    setTimeout(function(){tweenPlayer.to({x:260, y:300},700, 'Linear', true, 0);},0);
+    setTimeout(function(){tweenPlayer.to({x:220, y:300},700, 'Linear', true, 0);},0);
 
     player.animations.play('right');
 
