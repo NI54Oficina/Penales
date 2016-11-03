75c3a1097f0af5fc7f722829cb8b152f99b22439
Fran <fran@ni54.com>
2016-11-03 09:00:03 -0300

    08:59 03-11

--- tentativasServer.php
+++ tentativasServer.php
@@ -1,22 +1,42 @@
 <?php
 
-function UpdateStats($userId,$pateador,$arquero,$tendencia){
+//get stats en multiplayer que tenga tendencia
+
+//iniciar partida 
+
+//tabla historial
+//id, userid, partidaid, pateador, arquero, tipo
+
+//tabla partida
+//id (o token),idLocal, idVisitante, score=null, tipoPartida(single, multi o tipo de multi), apuesta
+
+function GetStats(){
+	
+}
+
+//quizas recibir el fin de partida?
+function UpdateStats($userId,$gameId,$pateador,$arquero,$tendencia){
+	//guardaria en db todos los datosa modo de historial y para asi desglozar los puntos de ser necesario
 	//pateador y aruqero son arrays con 0-1 segun si el user fue efectivo o no en sus respectivos turnos de ese modo
 	//tendencia = primeros 5 tiros como pateador a que indice fueron tirados, esto en singleplayer no se usaría
 	$auxPateador=0;
+	$puntos=0;
 	foreach($pateador as $p){
 		if($p==1){
+			$puntos+=1;
+			
 			$auxPateador++;
 			//convertidos++
-			//racha convertidos ++
-			//raca errados =0
+			//rachaConvertidos ++
+			//rachaErrados =0
 			//if(rachaConvertidos>rachaConvertidosHistorica){
 			//	rachaConvertidosHistorica=rachaConvertidos
 			//}
 		}else{
+			$puntos-=5;
 			//errados++
-			//racha convertidos=0
-			//racha errados ++
+			//rachaConvertidos=0
+			//rachaErrados ++
 			//if(rachaErrados>rachaErradosHistorica){
 			//	rachaErradosHistorica=rachaErrados
 			//}
@@ -25,6 +45,7 @@ function UpdateStats($userId,$pateador,$arquero,$tendencia){
 	$auxArquero=0;
 	foreach($arquero as $a){
 		if($a==1){
+			$puntos+=5;
 			//atajados++
 			//rachaNoAtajados=0;
 			//rachaAtajados++
@@ -32,6 +53,7 @@ function UpdateStats($userId,$pateador,$arquero,$tendencia){
 			//	rachaAtajadosHistorica= rachaAtajados
 			//}
 		}else{
+			$puntos-=1;
 			$auxArquero++;
 			//noAtajados++
 			//rachaAtajados=0;
@@ -40,6 +62,12 @@ function UpdateStats($userId,$pateador,$arquero,$tendencia){
 			//	rachaNoAtajadosHistorica= rachaNoAtajados
 			//}
 		}
+	}
+	if($auxPateador==count($pateador)){
+		$puntos+=5;
+	}
+	if($auxArquero==count($arquero)){
+		$puntos+=10;
 	}
 	if($auxPateador>$auxArquero){
 		//ganados++
@@ -56,7 +84,12 @@ function UpdateStats($userId,$pateador,$arquero,$tendencia){
 		//	rachaPerdidosHistorica= rachaPerdidos
 		//}
 	}
+	
+	GetStats();
 }
 
+function GetTendencia(){
+		
+}
 
 ?>
